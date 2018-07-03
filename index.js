016853410717cbc873fbb4d67de7906d56578abd
Karl G <karlg100@gmail.com>
2018-07-02 21:06:32 -0400

    clone of delay-switch framework for our smartstart plugin, and new package file

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+ 
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  
+  homebridge.registerAccessory("homebridge-smartstart", "SmartStart", SmartStart);
+}
+
+function SmartStart(log, config) {
+  this.log = log;
+  this.name = config.name;
+  this.delayTime = config.delay;
+  this.Timer;
+  this._service = new Service.Switch(this.name);
+  this._service.getCharacteristic(Characteristic.On)
+    .on('set', this._setOn.bind(this));
+}
+
+DelaySwitch.prototype.getServices = function() {
+  return [this._service];
+}
+
+DelaySwitch.prototype._setOn = function(on, callback) {
+ this.log("Smart starting " + on);
+ if (on) {
+    clearTimeout(this.Timer);
+    this.Timer = setTimeout(function() {
+      this._service.setCharacteristic(Characteristic.On, false);
+    }.bind(this), this.delayTime);
+  }
+  else { 
+   clearTimeout(this.Timer);
+  }
+  
+  callback();
+}
