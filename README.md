b75e6f8bbfcb00aecf2921df0c675586c6acf179
Karl G <karlg100@gmail.com>
2018-07-04 19:03:45 -0400

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# homebridge-viper
+
+# "Viper Smartstart" Plugin
+
+## Example config.json:
+
+ ```
+    "accessories": [
+        {
+            "accessory": "SmartStart",
+            "name": "Civic",
+            "username": "[usernmae]",
+            "password": "[password]",
+            "deviceIndex": 0,
+            "delay": 900000
+        }
+    ]
+
+```
+
+## Why do we need this plugin?
+
+This plugin enables your SmartStart vehicle to be controlled by HomeKit (and Siri) via homebridge.  Install using the standard homebridge instructions, and add this plugin.
+
+* name - give your vehicle a name.  Can be anything you like
+* username - your username you use to login to the app on your phone to control your vehicle
+* password - the password to the same account
+* deviceIndex - if you have one vehicle, us 0.  second vehcile is 1, thrid is 2, and so on.
+* delay - the default is a 15 min runtime.  This will turn the switch to the off position after 15 mins, unless you turn it off manually (and stop the car)
+
+## This is a as simple as it gets impliemntation
+
+Using the SmartStart npm module, this will just send a "remote" action to your vehicle.  There is no state inforamiton at this time that's accessable.  (somehow the app is able to retrieve the status of the vehicle, but it costs an "action" which accouts are limited to a number per year, and the API is not strait forward)
+
+Locking/unlocking is also not implimented.
+
+## How to install
+
+ ```sudo npm install -g homebridge-smartstart```
+ 
+## Credits
+This plugin was forked from and inspired by homebridge-delay-switch, homebridge-simple-switch-example and the alexa-smartstart and smartstart npm by @dale3h.  Without these last two modules, this homebridge module wouldn't exist.
--- index.js
+++ index.js
@@ -1,40 +1,115 @@
-"use strict";
+'use strict'
 
-var Service, Characteristic;
+let Service, Characteristic
+var SmartStartLib = require('smartstart');
 
-module.exports = function(homebridge) {
- 
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  
-  homebridge.registerAccessory("homebridge-smartstart", "SmartStart", SmartStart);
+
+module.exports = (homebridge) => {
+  /* this is the starting point for the plugin where we register the accessory */
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-smartstart', 'SmartStart', SmartStart)
 }
 
-function SmartStart(log, config) {
-  this.log = log;
-  this.name = config.name;
-  this.delayTime = config.delay;
-  this.Timer;
-  this._service = new Service.Switch(this.name);
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
-}
+class SmartStart {
+  constructor (log, config) {
+    // get config values
+    this.log = log;
+    this.name = config['name'];
+    this.service = new Service.Switch(this.name);
+    this.delay = config['delay'];
+    this.timer;
 
-SmartStart.prototype.getServices = function() {
-  return [this._service];
-}
+    // define SmartStart vehicle
+    this.vehicle = new SmartStartLib({
+      username: config['username'],
+      password: config['password'],
+      deviceIndex: config['deviceIndex']
+    });
 
-SmartStart.prototype._setOn = function(on, callback) {
- this.log("Smart starting " + on);
- if (on) {
-    clearTimeout(this.Timer);
-    this.Timer = setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), this.delayTime);
+    this.log(config['username']);
+
+    this.service = new Service.Switch(this.name);
   }
-  else { 
-   clearTimeout(this.Timer);
+
+  getServices () {
+    /*
+     * The getServices function is called by Homebridge and should return an array of Services this accessory is exposing.
+     * It is also where we bootstrap the plugin to tell Homebridge which function to use for which action.
+     */
+
+     /* Create a new information service. This just tells HomeKit about our accessory. */
+    const informationService = new Service.AccessoryInformation()
+        .setCharacteristic(Characteristic.Manufacturer, 'oznu')
+        .setCharacteristic(Characteristic.Model, 'SwitchExample')
+        .setCharacteristic(Characteristic.SerialNumber, 'oznu-switch-example')
+
+    /*
+     * For each of the service characteristics we need to register setters and getter functions
+     * 'get' is called when HomeKit wants to retrieve the current state of the characteristic
+     * 'set' is called when HomeKit wants to update the value of the characteristic
+     */
+    this.service.getCharacteristic(Characteristic.On)
+      .on('get', this.getOnCharacteristicHandler.bind(this))
+      .on('set', this.setOnCharacteristicHandler.bind(this))
+
+    /* Return both the main service (this.service) and the informationService */
+    return [informationService, this.service]
   }
-  
-  callback();
+
+  actionCallback(err, result) {
+    if(err) return console.error(err);
+    console.log(result);
+  }
+
+  setOnCharacteristicHandler (value, callback) {
+    /* this is called when HomeKit wants to update the value of the characteristic as defined in our getServices() function */
+
+    /*
+     * The desired value is available in the `value` argument.
+     * This is just an example so we will just assign the value to a variable which we can retrieve in our get handler
+     */
+    this.isOn = value
+
+    if (!value) {
+      clearTimeout(this.timer);
+    } else {
+
+      this.timer = setTimeout(function() {
+        this.log('Time is Up!');
+        this.service.getCharacteristic(Characteristic.On).updateValue(false);
+        this.isOn = false;
+      }.bind(this), this.delay);
+
+    }
+
+    /* Log to the console the value whenever this function is called */
+    this.log(`calling setOnCharacteristicHandler`, value)
+
+    this.vehicle.remote(this.actionCallback);
+    /*
+     * The callback function should be called to return the value
+     * The first argument in the function should be null unless and error occured
+     */
+    callback(null)
+  }
+
+  getOnCharacteristicHandler (callback) {
+    /*
+     * this is called when HomeKit wants to retrieve the current state of the characteristic as defined in our getServices() function
+     * it's called each time you open the Home app or when you open control center
+     */
+
+    /* Log to the console the value whenever this function is called */
+    this.log(`calling getOnCharacteristicHandler`, this.isOn)
+
+    /*
+     * The callback function should be called to return the value
+     * The first argument in the function should be null unless and error occured
+     * The second argument in the function should be the current value of the characteristic
+     * This is just an example so we will return the value from `this.isOn` which is where we stored the value in the set handler
+     */
+    callback(null, this.isOn)
+  }
+
 }
